edf8b7f4783784f75d21cb823f92a794109b960d
Dana <danachuchuk0@gmail.com>
2020-10-30 17:29:08 +0200

    user model fix

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,10 +1,8 @@
 export class User {
     id: string;
-    node_id: string;
     login: string;
     avatar_url: string;
     html_url: string;
-    created_at: string;
-    updated_at: string;
+    followers: string;
     public_repos: string;
 }
